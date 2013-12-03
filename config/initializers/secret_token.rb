c89bcc5c63ac7a7101d9ac2602ff56a095e34d36
emerson <lugaid@gmail.com>
2013-12-03 09:17:15 -0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailwaysAndStations::Application.config.secret_key_base = '3a9f86ee5001c4bc0e42b5c2a5eab24da0d04d98d6e668bc7cd8b6a8a79ebf48a5edb864762c5a0bdcbea31289fa39c9cefba171bfe9db6bf5acf30d5bf66bf2'
